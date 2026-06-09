73b8e2bf349bf5d99faeab2e8477a57a9e1f7fd9
nasa <c0a25172a8@edu.teu.ac.jp>
2026-06-09 15:57:25 +0900

    練習7と8と9

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,22 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png") #練習3
+    bg_img2 = pg.transform.flip(bg_img,True,False) #練習8背景二枚目反転
+    kk_img = pg.image.load("fig/3.png") #練習3こうかとん表示
     kk_img = pg.transform.flip(kk_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
-        screen.blit(bg_img, [-x, 0]) #練習5
-        screen.blit(kk_img, [300, 200]) #練習4
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0]) #練習5背景動くように
+        screen.blit(bg_img2, [-x+1600, 0]) #練習7背景残像の2枚目
+        screen.blit(bg_img, [-x+3200, 0]) #練習9背景の3枚目
+        screen.blit(kk_img, [300, 200]) #練習4こうかとん位置
         pg.display.update()
         tmr += 1        
-        clock.tick(200) #練習6
+        clock.tick(200) #練習6FPS変更
 
 
 if __name__ == "__main__":
